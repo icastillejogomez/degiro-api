7aaa7c75b1c9f63c99bab53209ea19e98e3e4819
Ignacio Castillejo <i.castillejogomez@gmail.com>
2020-05-15 05:25:19 +0200

    Updated project enums

--- src/lib/enums/DeGiroEnums.ts
+++ src/lib/enums/DeGiroEnums.ts
@@ -3,3 +3,38 @@ export enum MainDeGiroConsts {
   LOGIN_URL_PATH = 'login/secure/login',
   GET_ACCOUNT_CONFIG_PATH = 'login/secure/config',
 }
+
+export enum DeGiroActions {
+  BUY = 'BUY',
+  SELL = 'SELL',
+}
+
+export enum DeGiroOrderTypes {
+  LIMITED = 0,
+  MARKET = 2,
+  STOP_LOSS = 3,
+  STOP_LOSS_LIMIT = 1,
+}
+
+export enum DeGiroTimeTypes {
+  DAY = 1,
+  PERMANENT = 3,
+}
+
+export enum DeGiroProducTypes {
+  // all = undefined, undefined is not allowed to set on enum
+  shares = 1,
+  bonds = 2,
+  futures = 7,
+  options = 8,
+  investmendFunds = 13,
+  leveragedProducts = 14,
+  etfs = 131,
+  cfds = 535,
+  warrants = 536,
+}
+
+export enum DeGiroSort {
+  ASC = 'asc',
+  DESC = 'desc',
+}
